b3dabf8780a0a068fbdb6b3a294659b5ca6e919e
Rafael Castro <ralphc1992@live.com>
2024-08-07 22:44:13 -0500

    fresh look

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -24,21 +24,24 @@ export const Welcome: React.FC = () => {
   }, []);
   return (
     <>
-      <div className="relative z-40 pt-20 flex gap-20 mx-auto items-center justify-between w-[90%] max-w-[1200px]">
-        <div>
-          <img className="w-full h-full" src={softlogo} alt="" />
-        </div>
+      <div className="relative z-40 pt-20 flex flex-col md:flex-row gap-20 mx-auto items-center justify-between w-[90%] max-w-[1200px]">
         <div
           className={`p-4 transition-colors duration-300 ${
             scrolled ? "text-white" : "text-dark-green"
           }`}
         >
-          <h1 className="text-6xl font-bold">WELCOME</h1>
+          <h1 className="text-6xl font-bold mb-3">WELCOME</h1>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
-            cumque, quisquam beatae asperiores nihil molestiae. Libero nemo
-            sapiente impedit dignissimos!
+            I’m a web developer who’s all about creating sleek, interactive
+            websites. While I specialize in front-end development, I’m also into
+            building secure login systems and managing data with Firebase. I
+            love bringing ideas to life with clean designs and smooth user
+            experiences. Thanks for dropping by!
           </p>
+        </div>
+
+        <div className="w-full">
+          <img className="max-w-[300px]" src={softlogo} alt="" />
         </div>
       </div>
     </>
